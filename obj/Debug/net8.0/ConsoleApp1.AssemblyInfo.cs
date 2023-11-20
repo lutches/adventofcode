438be164c1cccdba2813712474ee7620dc6503d6
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-20 22:51:21 +0100

    commit

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1932e86fa0a3391a0e6857ca9662228bd0b1e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8a3bfdfae4eeadbddf631afe6bced41b2eee96")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
