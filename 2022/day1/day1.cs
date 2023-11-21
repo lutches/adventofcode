6a22a2b61f6b136b6f40fa8c79d044d3b8d85a94
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-21 23:30:59 +0100

    ugly but works

---
+++ 2022/day1/day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.IO;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+class day1
+{
+ static void Main()
+    {
+        string fileContent = File.ReadAllText("input.in");
+        //Console.WriteLine(fileContent);
+
+        string[] elfs = fileContent.Split("\n\n");
+        int[] elftotal = new int[elfs.Length];
+
+        for (int i = 0; i < elfs.Length; i++)
+        {
+            string[] elf = elfs[i].Split("\n");
+            int total = 0;
+            for (int j = 0; j < elf.Length; j++)
+            {
+                string[] ing = elf[j].Split(" ");
+                total += int.Parse(ing[ing.Length - 1]);
+            }
+            elftotal[i] = total;
+        }
+
+        Array.Sort(elftotal);
+        Array.Reverse(elftotal);
+        int topthree = elftotal[0] + elftotal[1] + elftotal[2];
+        foreach (int i in elftotal)
+            Console.WriteLine(i);
+
+        Console.WriteLine(topthree);
+    }
+
+}
---
+++ 2022/day2/day2/Program.cs
@@ -0,0 +1,27 @@
+﻿string input = File.ReadAllText("input.in");
+
+string[] rounds = input.Split("\n");
+
+int score = 0;
+
+foreach (string i in rounds)
+{
+    //win
+    if (i.Contains("Z"))
+    {
+        score += 6;
+    }
+    //draw
+    else if (i.Contains("Y"))
+    {
+        score += 3;
+    }
+}
+
+foreach (string i in rounds)
+{
+    if (i.Contains("A Y") ^ i.Contains("B X") ^ i.Contains("C Z")){score += 1;}
+    else if (i.Contains("B Y") ^ i.Contains("C X") ^ i.Contains("A Z")){score += 2;}
+    else {score += 3;}
+}
+Console.WriteLine(score);
