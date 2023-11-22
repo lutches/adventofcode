b82458a5289481a9b8d67ef83be2928e1d1b9698
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-22 15:22:43 +0100

    dag 3

--- 2022/day3/day3/Program.cs
+++ 2022/day3/day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Xml;
 
@@ -28,12 +29,37 @@ foreach (string bag in items)
                 int value = (int) i - 96;
                 output += value;
             }
-            Console.WriteLine(halfbag1.Length == halfbag2.Length);
+            //Console.WriteLine(halfbag1.Length == halfbag2.Length);
             break;
             
         }
         
     }
 }
-Console.WriteLine(output);
+//Console.WriteLine(output);
+int count = 0;
+runs = 0;
+additions = 0;
+for (int i = 0; i < items.Length; i += 3) {
+    runs ++;
+    foreach (char letter in items[i]){
 
+        if (items[i+1].Contains(letter) & items[i+2].Contains(letter)){
+            additions ++;
+            if (letter >= 'A' && letter <= 'Z'){
+                int value = (int) letter - 38;
+                count += value;
+            }
+            
+            else{
+                int value = (int) letter - 96;
+                count += value;
+            }
+            break;
+
+        }
+    }
+}
+Console.WriteLine(count);
+Console.WriteLine(runs);
+Console.WriteLine(additions);
