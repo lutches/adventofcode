6a7901569955c7940d87b5f10adcf4cdc805ae06
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-22 09:52:23 +0100

    worked on day3

--- 2022/day3/day3/Program.cs
+++ 2022/day3/day3/Program.cs
@@ -1,6 +1,37 @@
-﻿String[] items = File.ReadAllLines("input.in");
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml;
 
+String[] items = File.ReadAllLines("input.in");
+
+int output = 0;
 foreach (string bag in items)
 {
-    
+    int halfsize = bag.Length/2;
+
+    string halfbag1 = bag.Substring(0,halfsize-1);
+    string halfbag2 = bag.Substring(halfsize, halfsize-1);
+    if (halfbag1.Length == halfbag2.Length)
+    {
+        Console.WriteLine("True");
+    }
+    Console.WriteLine(halfbag1);
+    Console.WriteLine(halfbag2);
+    foreach (char i in halfbag1){
+        if (halfbag2.Contains(i))
+        {
+            Console.WriteLine(i);
+            if (i >= 'A' && i <= 'Z'){
+                int value = (int) i - 38;
+                output += value;
+            }
+            else{
+                int value = (int) i - 96;
+                output += value;
+            }
+            
+        }
+        
+    }
 }
+Console.WriteLine(output);
--- 2022/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
+++ 2022/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a22a2b61f6b136b6f40fa8c79d044d3b8d85a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0987bedf87264d0870abf18d7ada47df3249b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("day3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
