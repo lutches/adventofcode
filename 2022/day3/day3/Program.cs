7b3b829b01da7b8d9997379ae85110e4ec79617b
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-22 14:47:49 +0100

    day3.1

--- 2022/day3/day3/Program.cs
+++ 2022/day3/day3/Program.cs
@@ -5,33 +5,35 @@ using System.Xml;
 String[] items = File.ReadAllLines("input.in");
 
 int output = 0;
+int runs = 0;
+int additions = 0;
+char[] outputchars = [];
 foreach (string bag in items)
 {
     int halfsize = bag.Length/2;
 
-    string halfbag1 = bag.Substring(0,halfsize-1);
-    string halfbag2 = bag.Substring(halfsize, halfsize-1);
-    if (halfbag1.Length == halfbag2.Length)
-    {
-        Console.WriteLine("True");
-    }
-    Console.WriteLine(halfbag1);
-    Console.WriteLine(halfbag2);
+    string halfbag1 = bag.Substring(0,halfsize);
+    string halfbag2 = bag.Substring(halfsize, halfsize);
+    runs ++;
     foreach (char i in halfbag1){
         if (halfbag2.Contains(i))
         {
-            Console.WriteLine(i);
+            additions ++;
             if (i >= 'A' && i <= 'Z'){
                 int value = (int) i - 38;
                 output += value;
             }
+            
             else{
                 int value = (int) i - 96;
                 output += value;
             }
+            Console.WriteLine(halfbag1.Length == halfbag2.Length);
+            break;
             
         }
         
     }
 }
 Console.WriteLine(output);
+
--- 2022/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
+++ 2022/day3/day3/obj/Debug/net8.0/day3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7901569955c7940d87b5f10adcf4cdc805ae06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df41e9566094f0adf335281d90c40e98a7c027db")]
 [assembly: System.Reflection.AssemblyProductAttribute("day3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
