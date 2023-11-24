9ff257760b6536b09e5df67789c6080aeb1fa571
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-24 15:42:06 +0100

    stupid but efficient

---
+++ 2022/day6/day6/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+string data = File.ReadAllText("input.in");
+
+int output = 0;
+Console.WriteLine(data.Length);
+
+for (int i = 3; i < data.Length; i ++){
+
+    if (data[i-3] != data [i-2] 
+    && data[i-3] != data[i-1] 
+    && data[i-3] != data[i] 
+    && data[i-2] != data[i-1] 
+    && data[i-2] != data[i] 
+    && data[i-1] != data[i]){
+
+                output = i+1;
+                break;
+            }
+        }
+
+
+
+Console.WriteLine(output);
