7de31e19f0fab4ed328a99c86175fec4c0237944
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-11-24 16:03:28 +0100

    day6 part two

--- 2022/day6/day6/Program.cs
+++ 2022/day6/day6/Program.cs
@@ -4,20 +4,24 @@ string data = File.ReadAllText("input.in");
 
 int output = 0;
 Console.WriteLine(data.Length);
+HashSet<char> comps = [];
 
-for (int i = 3; i < data.Length; i ++){
+for (int i = 13; i < data.Length; i ++){
 
-    if (data[i-3] != data [i-2] 
-    && data[i-3] != data[i-1] 
-    && data[i-3] != data[i] 
-    && data[i-2] != data[i-1] 
-    && data[i-2] != data[i] 
-    && data[i-1] != data[i]){
+    for (int j = 0; j < 14; j++){
+        comps.Add(data[i-j]);
+    }
 
-                output = i+1;
-                break;
-            }
-        }
+    if (comps.Count == 14){
+        Console.WriteLine(comps.ToArray());
+        output = i+1;
+        break;
+    }
+    comps.Clear();
+    
+    
+}  
+
 
 
 
--- 2022/day6/day6/obj/Debug/net8.0/day6.AssemblyInfo.cs
+++ 2022/day6/day6/obj/Debug/net8.0/day6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bbc01cc0476651242aba76b798d448f083b5ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff257760b6536b09e5df67789c6080aeb1fa571")]
 [assembly: System.Reflection.AssemblyProductAttribute("day6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
