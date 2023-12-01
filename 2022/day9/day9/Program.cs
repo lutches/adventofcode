663b59df5cecc2d2736f11dc805125dac504c0b9
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2023-12-01 20:32:57 +0100

    day1 2023

--- 2022/day9/day9/Program.cs
+++ 2022/day9/day9/Program.cs
@@ -1,8 +1,15 @@
-﻿string[] instructions = File.ReadAllLines("input.txt");
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
+string[] instructions = File.ReadAllLines("input.txt");
 
 
 HashSet<(int,int)> places = [];
 
+HashSet<(int,int)> headplaces = [];
+/*
 (int,int) distance = (0,0);
 
 (int,int) tailcord = (0,0);
@@ -15,7 +22,7 @@ foreach (string instruction in instructions){
     for (int steps = 0; steps < Int32.Parse(inst.Last()); steps ++){
 
 
-        places.Add(tailcord);
+        
         
 
         string direction = inst.First();
@@ -59,9 +66,81 @@ foreach (string instruction in instructions){
             }
             
         }
+
+        places.Add(tailcord);
     }
 }
 
 Console.WriteLine(places.Count);
 
+*/
+static (int,int) moveafter((int,int) previous,(int,int) current){
+    if(Math.Abs(previous.Item1 - current.Item1)> 1 || Math.Abs(previous.Item2-current.Item2)>1){
 
+        (int,int) distance = (previous.Item1-current.Item1, previous.Item2-current.Item2);
+        (int,int) response = (0,0);
+
+        if (distance.Item1 > 1){
+            response.Item1 = current.Item1 + 1;
+            response.Item2 = previous.Item2; 
+        }
+        if (distance.Item1 < -1){
+            response.Item1 = current.Item1 - 1;
+            response.Item2 = previous.Item2;
+        }
+        if (distance.Item2 > 1){
+            response.Item2 = current.Item2 + 1;
+            response.Item1 = previous.Item1;
+        }
+        if (distance.Item2 < -1) {
+            response.Item2 = current.Item2 - 1;
+            response.Item1 = previous.Item1;
+        }
+
+        return response;
+    }
+    else {return current;}
+}
+
+
+int length = 10;
+List<(int,int)> rope = [];
+for (int i = 0; i < length; i++){
+        rope.Add((0,0));
+    }
+
+foreach (string instruction in instructions){
+    string[] inst = instruction.Split(' ');
+    
+    
+
+    for (int steps = 0; steps < Int32.Parse(inst.Last()); steps ++){
+        string direction = inst.First();
+
+        if (direction == "R"){
+            rope[0] = (rope[0].Item1 + 1, rope[0].Item2);
+        }
+        if (direction == "L"){
+            rope[0] = (rope[0].Item1 - 1, rope[0].Item2);
+        }
+        if (direction == "U"){
+            rope[0] = (rope[0].Item1, rope[0].Item2 + 1);
+        }
+        if (direction == "D"){
+            rope[0] = (rope[0].Item1, rope[0].Item2 - 1);
+        }
+
+        for (int knot = 1; knot < rope.Count; knot++){
+            rope[knot] = moveafter(rope[knot-1],rope[knot]);
+        }
+        places.Add(rope[^1]);
+
+    }
+    
+    
+    
+}
+Console.WriteLine(places.Count);
+
+//2583 is to high
+//not off by one error
---
+++ 2023/day1/day1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text;
+
+string[] data = File.ReadAllLines("input.in");
+
+int sum = 0;
+
+
+foreach(string line in data){
+    int[] thisline = findnumber(line);
+    List<int> lineints = [];
+    lineints.Add(thisline[0]);
+    lineints.Add(thisline[^1]);
+    
+ 
+
+    StringBuilder str = new StringBuilder("",2);
+    str.Append(lineints[0]);
+    str.Append(lineints[1]);
+    string String = str.ToString();
+    sum += int.Parse(String);
+}
+Console.WriteLine(sum);
+
+
+static int[] findnumber(string line){
+    List<int> numbers = [];
+    string[] letternumbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+    for (int letter = 0; letter < line.Length; letter++){
+    //every char
+        for (int number = 0; number < letternumbers.Length; number++){
+            //compare with numbers
+            if (letternumbers[number][0] == line[letter]){
+                for (int nextletter = 1; nextletter< 10; nextletter ++){
+                    if (letter+nextletter < line.Length){
+                        if (letternumbers[number][nextletter] == line[letter+nextletter]){
+                            if (nextletter >= letternumbers[number].Length-1){
+                                numbers.Add(number);
+                                break;
+                                }
+                        }
+                        else {break;}
+                    }
+                }
+                    
+                }
+            }
+        if(line[letter]-48 < 10){numbers.Add(line[letter]-48);}
+            
+            
+            }
+
+            int[] arr = numbers.ToArray();
+            return arr;
+        }
+
+
+
